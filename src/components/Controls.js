963d548668b6cf831ba8c83051c54e70a47a07f4
CapsE <larskrafft@yahoo.de>
2017-07-15 16:14:53 +0200

    Slider angepasst Icons hinzugefügt

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -6,10 +6,14 @@ import IconSlider from './Slider';
 import 'rc-slider/assets/index.css';
 
 class Controls extends React.Component {
+  test(e){
+    console.log(e);
+  }
   render() {
+    let icon = require("../images/audio.svg");
     return (
       <div className="controls">
-        <IconSlider/>
+        <IconSlider icon={icon} onChange={this.test}/>
         <Button>SOLO</Button>
       </div>
     );
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,18 +4,22 @@ import React from 'react';
 import Slider from 'rc-slider';
 
 class IconSlider extends React.Component {
+  changeEvent(e){
+    this.props.onChange(e);
+  }
+
   render() {
     return (
       <div className="icon-slider">
-        <Slider min={0} max={100} />
-        <img className="icon"/>
+        <Slider min={0} max={100} defaultValue={50} onChange={this.changeEvent.bind(this)} />
+        <img className="icon" src={this.props.icon} />
       </div>
     );
   }
 }
 
 IconSlider.defaultProps = {
-
+  icon: require("../images/audio.svg")
 };
 
 export default IconSlider;
